2a21e1f160b8816b78fcf7ac18aea60c553b7a55
JoseNogueira37 <JoseNogueira.014.0@gmail.com>
2022-02-22 12:18:38 -0300

    commit

--- aula01/modelo/function.php
+++ aula01/modelo/function.php
@@ -12,17 +12,17 @@
 
         switch($ANIMAL){
             case '1' : $dados = array(
-                "tipo" => 'cachorro',
+                "tipo" => 'cachorro.jpg',
                 "mensagem" => 'Seja bem vindo, '.$NOME', sabemos que seu animal preferido é o cachorro' 
             );
             break;
             case '2' : $dados = array(
-                "tipo" => 'cachorro',
+                "tipo" => 'gato.jpg',
                 "mensagem" => 'Seja bem vindo, '.$NOME', sabemos que seu animal preferido é gato' 
             );
             break;
             case '3' : $dados = array(
-                "tipo" => 'cachorro',
+                "tipo" => 'peixe.jpg',
                 "mensagem" => 'Seja bem vindo, '.$NOME', sabemos que seu animal preferido é o peixe' 
             );
             break;
