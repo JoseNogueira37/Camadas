85b7f83ed66d9834ee0edfb499a22f245f34515c
JoseNogueira37 <JoseNogueira.014.0@gmail.com>
2022-02-22 11:08:28 -0300

    commit

---
+++ aula01/modelo/function.php
@@ -0,0 +1,33 @@
+<?php
+
+    $NOME = $_REQUEST['NOME'];
+    $ANIMAL = $_REQUEST['ANIMAL'];
+
+    if(empty($NOME)){
+        $dados = array(
+            "tipo" => 'error',
+            "mensagem" => 'Existe(m) campos(s) a ser(em) preenchido(s).'
+        );
+    }else{
+
+        switch($ANIMAL){
+            case '1' : $dados = array(
+                "tipo" => 'cachorro',
+                "mensagem" => 'Seja bem vindo, '.$NOME', sabemos que seu animal preferido é o cachorro' 
+            );
+            break;
+            case '2' : $dados = array(
+                "tipo" => 'cachorro',
+                "mensagem" => 'Seja bem vindo, '.$NOME', sabemos que seu animal preferido é gato' 
+            );
+            break;
+            case '3' : $dados = array(
+                "tipo" => 'cachorro',
+                "mensagem" => 'Seja bem vindo, '.$NOME', sabemos que seu animal preferido é o peixe' 
+            );
+            break;
+            
+        }
+
+    }
+    echo json_encode($dados);
